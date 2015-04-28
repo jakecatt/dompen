54619ddce601df7878dc3c967dbe86224a2a80a0
Nick Stares <nickstares0@gmail.com>
2015-04-28 15:54:36 -0700

    fancybox + commenting out mapbox

--- public/script/script.js
+++ public/script/script.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+
+$('.fancybox-media').on("click", function(e){
+console.log("FOOBAR")
+})
+
+
   //navbar and scrolling fixed position
   $(window).scroll(function(e) {
     var scroll = $(this).scrollTop();
@@ -10,114 +16,130 @@ $(document).ready(function(){
   });
 
   //slow scrolling to section when clicking on navbar link
-  $('a').on('click', function(e){
+  $('.product-link').on('click', function(e){
+console.log("navbar link is being clicked" )
     e.preventDefault();
     var target = findTarget($(this));
+
+
     console.log(target)
+
+    target[0].scrollIntoView({block: "start", behavior: "smooth"})
+  })
+
+  $('.navbar-link').on('click', function(e){
+    console.log("navbar link is being clicked" )
+    var target = findTarget($(this));
+
+
     $('html, body').stop().animate({
-      scrollTop: target.offset().top - 60
+       scrollTop: target.offset().top - 60
     }, 1000);
   })
 
+// START MAPBOX ======================================
+
   //display locations stored in the locations.js file
-  showLocationsList();
+  // showLocationsList();
 
-  //MAPBOX setup
-  L.mapbox.accessToken = 'pk.eyJ1IjoicGFtLSIsImEiOiJNT09NSzgwIn0.AWl1AY_kO1HMnFHwxb9mww';
-  var map = L.mapbox.map('map', 'pam-.d97b92e0', {})
+  // //MAPBOX setup
+  // L.mapbox.accessToken = 'pk.eyJ1IjoicGFtLSIsImEiOiJNT09NSzgwIn0.AWl1AY_kO1HMnFHwxb9mww';
+  // var map = L.mapbox.map('map', 'pam-.d97b92e0', {})
 
 
-  // if you're on mobile, make is so that you can't drag the map
-  if ($(document).width() < 600) {
-    map.dragging.disable();
-  }
+  // // if you're on mobile, make it so that you can't drag the map
+  // if ($(document).width() < 600) {
+  //   map.dragging.disable();
+  // }
   
-  //disables scrolling as zoom on the map so that when you are scrolling
-  //down the page you don't zoom in on the map
-  map.scrollWheelZoom.disable();
+  // //disables scrolling as zoom on the map so that when you are scrolling
+  // //down the page you don't zoom in on the map
+  // map.scrollWheelZoom.disable();
 
 
-  if (map.tap) {
-    map.tap.disable();
-  }
+  // if (map.tap) {
+  //   map.tap.disable();
+  // }
 
-  // location where the map renders
-  var geocoder = L.mapbox.geocoder('mapbox.places')
-  geocoder.query('Los Angeles, CA', showMap);
+  // // location where the map renders
+  // var geocoder = L.mapbox.geocoder('mapbox.places')
+  // geocoder.query('Los Angeles, CA', showMap);
 
-  //getting the locations from the locations.js file and rendering them
-  showMarkers();
+  // //getting the locations from the locations.js file and rendering them
+  // showMarkers();
 
 
-  function showMap(err, data) {
-    if (data.lbounds) {
-      var coords = latLng(data.lbounds)	
-      // the second argument of setView is how far the map is zoomed in
-      map.setView(coords, 10)
+  // function showMap(err, data) {
+  //   if (data.lbounds) {
+  //     var coords = latLng(data.lbounds)	
+  //     // the second argument of setView is how far the map is zoomed in
+  //     map.setView(coords, 10)
 
-    }
-  }
+  //   }
+  // }
   
-  //loops through all of the locations in locations.js and shows the markers for each one
-  function showMarkers() {
-    for (var i = 0; i < locations.length; i++) {
-      var location = locations[i];
-      var name = location.name;
-      var address = location.streetAddress.replace(/ /g, '+');
-      var area = location.zip;
-      var phone = location.phone;
-      if (address.length > 1) { 
-        var validAddress = address;
-      } else { 
-        validAddress = area 
-      }
-      var geocodeUrl = 'https://api.tiles.mapbox.com/v4/geocode/mapbox.places/'+ validAddress +'.json?access_token=pk.eyJ1IjoicGFtLSIsImEiOiJNT09NSzgwIn0.AWl1AY_kO1HMnFHwxb9mww'
-      $.ajax({
-        type: "GET",
-        url: geocodeUrl,
-        "name": name,
-        "phone": phone,
-        success: function(result) {
-          var coords = result.features[0]
-          renderMarkers(coords, this.name, this.phone)
-        }
-      })
-    }
-  }
+  // //loops through all of the locations in locations.js and shows the markers for each one
+  // function showMarkers() {
+  //   for (var i = 0; i < locations.length; i++) {
+  //     var location = locations[i];
+  //     var name = location.name;
+  //     var address = location.streetAddress.replace(/ /g, '+');
+  //     var area = location.zip;
+  //     var phone = location.phone;
+  //     if (address.length > 1) { 
+  //       var validAddress = address;
+  //     } else { 
+  //       validAddress = area 
+  //     }
+  //     var geocodeUrl = 'https://api.tiles.mapbox.com/v4/geocode/mapbox.places/'+ validAddress +'.json?access_token=pk.eyJ1IjoicGFtLSIsImEiOiJNT09NSzgwIn0.AWl1AY_kO1HMnFHwxb9mww'
+  //     $.ajax({
+  //       type: "GET",
+  //       url: geocodeUrl,
+  //       "name": name,
+  //       "phone": phone,
+  //       success: function(result) {
+  //         var coords = result.features[0]
+  //         renderMarkers(coords, this.name, this.phone)
+  //       }
+  //     })
+  //   }
+  // }
 
 
-  function renderMarkers(data, collectiveName, phoneNumber) {
-    var myLayer = L.mapbox.featureLayer().addTo(map);
-    var lat = data.center[1]
-    var lng = data.center[0]
+  // function renderMarkers(data, collectiveName, phoneNumber) {
+  //   var myLayer = L.mapbox.featureLayer().addTo(map);
+  //   var lat = data.center[1]
+  //   var lng = data.center[0]
 
-    var geoJson = [{
-      "type": "Feature",
-      "geometry": {
-	"type": "Point",
+  //   var geoJson = [{
+  //     "type": "Feature",
+  //     "geometry": {
+  //       "type": "Point",
 
-        "coordinates": [lng, lat]
-      },
-      "properties": {
-        "title": collectiveName +  " <br> " + data.place_name + "<br>" + phoneNumber,
-        "icon": {
-          "iconUrl": "images/DomPen_Icons/DomPenLogo_forMap.png", //INSERT PATH TO LOGO HERE
-          "iconSize": [50, 50], // size of the icon
-          "iconAnchor": [25, 25], // point of the icon which will correspond to marker's location
-          // "popupAnchor": [0, -25], // point from which the popup should open relative to the iconAnchor
-          "className": "dot"
-        }
-      }
-    }]
-    myLayer.on('layeradd', function(e) {
-      var marker = e.layer,
-    	  feature = marker.feature;
-      marker.setIcon(L.icon(feature.properties.icon));
-    });
+  //       "coordinates": [lng, lat]
+  //     },
+  //     "properties": {
+  //       "title": collectiveName +  " <br> " + data.place_name + "<br>" + phoneNumber,
+  //       "icon": {
+  //         "iconUrl": "images/DomPen_Icons/DomPenLogo_forMap.png", //INSERT PATH TO LOGO HERE
+  //         "iconSize": [50, 50], // size of the icon
+  //         "iconAnchor": [25, 25], // point of the icon which will correspond to marker's location
+  //         // "popupAnchor": [0, -25], // point from which the popup should open relative to the iconAnchor
+  //         "className": "dot"
+  //       }
+  //     }
+  //   }]
+  //   myLayer.on('layeradd', function(e) {
+  //     var marker = e.layer,
+  //   	  feature = marker.feature;
+  //     marker.setIcon(L.icon(feature.properties.icon));
+  //   });
 
-    // Add features to the map.
-    myLayer.setGeoJSON(geoJson);		
-  }
+  //   // Add features to the map.
+  //   myLayer.setGeoJSON(geoJson);		
+  // }
+
+// ----------- END MAPBOX ------------------
 
   // Setting the number of slides shown in the carousel depending on screen size 
   // setting various heights and widths depending on screen size
@@ -224,36 +246,38 @@ $(document).ready(function(){
 
 });
 
+// BEGIN MORE MAPBOX
+// function latLng(data) {
+//   var lat = (data._southWest.lat + data._northEast.lat)/2;
+//   var lng = (data._southWest.lng + data._northEast.lng)/2;
+//   return [lat, lng];
+// }
 
-function latLng(data) {
-  var lat = (data._southWest.lat + data._northEast.lat)/2;
-  var lng = (data._southWest.lng + data._northEast.lng)/2;
-  return [lat, lng];
-}
+// //shows a list of the locations on the sidebar
+// function showLocationsList() {
+//   for (var i = 0; i < locations.length; i++) {
+//     var location = locations[i];
+//     var area = location.area;
+//     $('.' + area).append(displayLocation(location.name, location.streetAddress, location.city, location.state, location.zip, location.phone));
+//   };
+// }
 
-//shows a list of the locations on the sidebar
-function showLocationsList() {
-  for (var i = 0; i < locations.length; i++) {
-    var location = locations[i];
-    var area = location.area;
-    $('.' + area).append(displayLocation(location.name, location.streetAddress, location.city, location.state, location.zip, location.phone));
-  };
-}
+// function displayLocation(name, street, city, state, zip, phone) {
+//   return '<div class="location"><h4>' 
+//     + name + '</h4><p><span class="street">'
+//     + street + '<span>'
+//     + city + ', ' 
+//     + state + ' ' + zip +
+//     ' ' + '<br>' + phone
+//   '</p>'
+// }
 
-function displayLocation(name, street, city, state, zip, phone) {
-  return '<div class="location"><h4>' 
-    + name + '</h4><p><span class="street">'
-    + street + '<span>'
-    + city + ', ' 
-    + state + ' ' + zip +
-    ' ' + '<br>' + phone
-  '</p>'
-}
+// function findTarget(element) {
+//   var name = element.attr('href').replace(/#/, '');
+//   return $('a[name="' + name + '"]')
+// }
 
-function findTarget(element) {
-  var name = element.attr('href').replace(/#/, '');
-  return $('a[name="' + name + '"]')
-}
+// END MORE MAPBOX
 
 $(".signup").on("submit", function(e){
 
